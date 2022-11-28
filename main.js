5d2df6da1a737001023a15bc32fbd719fa37d1b6
Víctor Peña <victortkd88@gmail.com>
2022-11-28 16:23:47 -0600

    new properties

--- main.js
+++ main.js
@@ -1,102 +1,71 @@
+// -------------- Ejercicio 1 --------------
 
-// ------------------ Ejercicio 1 ------------------
+// const string = 'freeCodeCamp'
+// let palabra = '';
 
-// const numeros = [1, 4, 15.5, -3, 7, -1, 15];
+// const revertir_string = (value) => {
+//   console.log(i);
+//   if (value < 0) return;
+//   palabra += string[value];
 
-const numeros = [-2, -3, -1.5, -4];
+//   return revertir_string(value - 1);
+// }
 
-function numero_mayor (num) {
-  let mayor = 0;
-  mayor = num[0]
+// revertir_string(string.length - 1);
+// console.log(palabra);
 
-  for (let i = 0; i < num.length; i++) {
-    
-    if (num[i] > mayor) {
-      mayor = num[i];
-    }
-  }
-  return mayor
-}
-console.log(numero_mayor(numeros));
+// -------------- Ejercicio 2 --------------
 
+// let contador = 0, bandera = 0;
+// let palabra = 'construccion', letra = 'c';
 
-
-// ------------------ Ejercicio 2 ------------------
-// let x = '';
-
-// function palidromo(string) {
-//   x = string;
-//   const palabra = x.toLowerCase();
-//   const palabra_invertida = palabra.split('').reverse().join('');
+// const recorrer = (n, str, l) => {
+//   if (n > str.length - 1) return;
   
-//   if (palabra === palabra_invertida) {
-//    return `La palabra "${string}" => "${palabra_invertida}" es un palídromo`;
-//   } else {
-//     return `La palabra "${string}" => "${palabra_invertida}", no es un palídromo.`;
+//   if (l === str[n]){
+//     contador++;
+//     bandera = 1;
 //   }
+
+//   return recorrer(n + 1, str, l), str, l;
 // }
-// console.log(palidromo('Apa'));
+
+// recorrer(0, palabra, letra)
+
+// if (bandera === 1) {
+//   console.log(`En la palabra ${palabra} aparece la letra '${letra}' ${contador} veces.`);
+// } else {
+//   console.log(`En la palabra ${palabra} no se encuentra la letra '${letra}'`);
+// }
 
 
-// ------------------ Ejercicio 3 ------------------
 
-// const matriz = [ [1,0,0], [0,1,1], [0,1,0] ];
+// -------------- Ejercicio 3 --------------
 
-// function encontrar_ceros (num) {
-//   let ceros_encontrados = 0;
+// const vocales = 'aeiou',
+//       palabra = 'esta es la entrada';
 
-//   for(let i = 0; i < num.length; i++) {
+// let nueva_palabra = '';
 
-//     for (let j = 0; j < num.length; j++) {
-//       if (num[i][j] === 0) ceros_encontrados++;
+
+// let arr = [], repetidos = [], contador = -1;
+// const recorrer = (n,vcl, str) => {
+//   if (n >= str.length ) return;
+//   contador++;
+//   arr[contador] = str[contador]
+  
+//   let i = 0;
+//   do {
+//     if(vcl[n] === str[i]) {
+//       repetidos[contador] = st[i]
 //     }
+//     i++
+//   } while(i < str.length)
+  
+//   if (arr.length === palabra.length) {
+//     console.log(arr);
+//     return;
 //   }
-//   return ceros_encontrados;
+//   return recorrer(n + 1,vcl, str );
 // }
-// console.log(encontrar_ceros(matriz));
-
-
-
-// ------------------ Ejercicio 4 ------------------
-
-// const numeros = [-1, 1, 2, 3, 1, -2, 4, 5, 4, 6, 7, -2, 8, 9, 10, -1, 10];
-
-// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-
-// function encontrar_repetidos(array) {
-//   let repetidos = [], bandera = 0;
-
-//   for (let i = 0; i < array.length; i++) {
-
-//     for (let j = i + 1; j < array.length; j++) {
-//       if (array[i] === array[j]) {
-//         repetidos.push(array[i])
-//         bandera = 1;
-//       }
-//     }
-//   }
-
-//   if (bandera === 0) {
-//     return 'No se encontraron números repetidos en la lista de números recibida.';
-//   } else {
-//     return repetidos;
-//   }
-// }
-// console.log(encontrar_repetidos(numeros));
-
-
-
-// ------------------ Ejercicio 5 ------------------
-
-// function piramide(rep) {
-
-//   for (let i = 0; i < rep; i++) {
-
-//     let elemento = '';
-//     for (let j = 0; j < i; j++) elemento += '* ';
-//     console.log(elemento);
-
-//   }
-// }
-// piramide(8);
+// recorrer(0,vocales, palabra)
